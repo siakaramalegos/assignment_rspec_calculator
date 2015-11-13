f63b1a80fcbc12d4a631223bc3aea09cce69a36d
Sia <siakaramalegos@gmail.com>
2015-11-13 16:33:33 -0600

    Fixes calculator let location and tests add

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,9 +2,9 @@ require_relative '../lib/calculator.rb'
 
 describe Calculator do
 
-  describe '#initialize' do
+  let(:calculator){Calculator.new}
 
-    let(:calculator){Calculator.new}
+  describe '#initialize' do
 
     it 'should give you a new calculator object' do
       expect(calculator).to be_a(Calculator)
@@ -12,10 +12,10 @@ describe Calculator do
 
   end
 
-  # describe '.add' do
+  describe '.add' do
 
-  #   it 'should add two numbers' do
-  #     expect()
-  #   end
-  # end
+    it 'should add two numbers' do
+      expect(calculator.add(1, 2)).to eq(3)
+    end
+  end
 end
