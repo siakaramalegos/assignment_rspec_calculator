263c308b6ddd508cf9918eb5a578834792b78c2c
Sia <siakaramalegos@gmail.com>
2015-11-13 16:30:12 -0600

    Adds calculator initialize spec

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,21 @@
-# Your code here
+require_relative '../lib/calculator.rb'
+
+describe Calculator do
+
+  describe '#initialize' do
+
+    let(:calculator){Calculator.new}
+
+    it 'should give you a new calculator object' do
+      expect(calculator).to be_a(Calculator)
+    end
+
+  end
+
+  # describe '.add' do
+
+  #   it 'should add two numbers' do
+  #     expect()
+  #   end
+  # end
+end
